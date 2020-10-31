c9d1dcb6a4e61588387168ac2244e50ae7e93a85
davidchenbest <davidchen108@gmail.com>
2020-10-31 18:04:17 -0400

    make it better

--- server.js
+++ server.js
@@ -21,7 +21,9 @@ app.use(authRoutes)
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'))
-    
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname, client,build,'./public/index.html'))
+    })
 }
 
 const whitelist = ['http://localhost:3000', 'http://localhost:5000', 'https://jiachen.herokuapp.com/']
