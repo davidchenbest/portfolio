06b2f53ac4a57cc765133f4e19c9822ce838063d
davidchenbest <davidchen108@gmail.com>
2021-02-16 15:46:46 -0500

    gallery: navigator on keypress left/right

--- client/src/components/gallery/Navigator.js
+++ client/src/components/gallery/Navigator.js
@@ -1,9 +1,29 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState,useCallback } from 'react'
 import '../../css/navigator.css'
 
 export default function Navigator({ currentPhoto, folderState, singleState }) {
     const [currentIndex, setCurrentIndex] = useState(0)
     const [currentLink, setCurrentLink] = useState('')
+
+    const setStateLeftRight = useCallback((e, step) => {
+        if(e) e.stopPropagation()
+        let index = currentIndex + step
+        if (index === -1) index = currentPhoto.folderObj.photos.length - 1
+        else if (index === currentPhoto.folderObj.photos.length) index = 0
+        setCurrentIndex(index)
+        setCurrentLink(currentPhoto.folderObj.photos[index].photoLink);
+    },[currentIndex,currentPhoto.folderObj.photos])
+
+    const keydownEvent = useCallback(e=>{
+        console.log(0);
+        if (e.key === 'ArrowRight') setStateLeftRight(null,1)
+        else if (e.key === 'ArrowLeft') setStateLeftRight(null,1)
+    },[setStateLeftRight])
+
+    useEffect(() => {
+        window.addEventListener('keydown', keydownEvent)
+        return () => window.removeEventListener("keydown",keydownEvent);
+    }, [keydownEvent])
 
     useEffect(() => {
         let photos = currentPhoto.folderObj.photos
@@ -18,14 +38,6 @@ export default function Navigator({ currentPhoto, folderState, singleState }) {
         singleState.setSingleMode(!singleState.singleMode)
     }
 
-    const setStateLeftRight = (e, step) => {
-        e.stopPropagation()
-        let index = currentIndex + step
-        if (index === -1) index = currentPhoto.folderObj.photos.length - 1
-        else if (index === currentPhoto.folderObj.photos.length) index = 0
-        setCurrentIndex(index)
-        setCurrentLink(currentPhoto.folderObj.photos[index].photoLink);
-    }
 
     return (
         <div className='singleView' onClick={e => exitClick(e)} >
